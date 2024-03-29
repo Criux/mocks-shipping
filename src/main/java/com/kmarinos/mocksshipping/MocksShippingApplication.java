b25817457913366bad6a77dead792a7b589b3d0c
Criux <greekmule@gmail.com>
2024-03-29 17:04:19 +0100

    init

---
+++ src/main/java/com/kmarinos/mocksshipping/MocksShippingApplication.java
@@ -0,0 +1,13 @@
+package com.kmarinos.mocksshipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocksShippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocksShippingApplication.class, args);
+	}
+
+}
